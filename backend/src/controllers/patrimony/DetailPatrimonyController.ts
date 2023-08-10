5baff992703c4930003bd684d2b6f804eb35b03b
mv23c <100789090+mv23c@users.noreply.github.com>
2023-08-10 18:42:55 -0300

    Finalização do serviço de verificar detalhes de um patrimônio

--- backend/src/controllers/patrimony/DetailPatrimonyController.ts
+++ backend/src/controllers/patrimony/DetailPatrimonyController.ts
@@ -3,12 +3,13 @@ import { DetailPatrimonyService } from "../../services/patrimony/DetailPatrimony
 
 class DetailPatrimonyController {
   async handle(req: Request, res: Response) {
+    const id = req.query.id as string;
+
     const detailPatrimonyService = new DetailPatrimonyService();
 
-    const patrimony = await detailPatrimonyService.execute();
+    const patrimony = await detailPatrimonyService.execute({ id });
 
     return res.json(patrimony);
-
   }
 }
 
--- backend/src/services/patrimony/DetailPatrimonyService.ts
+++ backend/src/services/patrimony/DetailPatrimonyService.ts
@@ -1,8 +1,33 @@
 import prismaClient from "../../prisma";
 
+interface PatrimonyRequest {
+  id: string;
+}
+
 class DetailPatrimonyService {
-  async execute() {
-    return { ok: true }
+  async execute({ id }: PatrimonyRequest) {
+    if(!id) {
+      throw new Error("invalid id");
+    }
+
+    const patrimony = await prismaClient.patrimony.findFirst({
+      where: {
+        id: id,
+      },
+      select: {
+        id: true,
+        Name: true,
+        Tombamento: true,
+        Status: true,
+        Detentor: true,
+      }
+    })
+
+    if (patrimony !== null) {
+      return patrimony;
+    } else {
+      throw new Error("There is no patrimony with that id")
+    }
   }
 }
 
